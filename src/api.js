870f2cf9b3eed563fdfca64a455450b704637ff0
Lupino <lmjubuntu@gmail.com>
2017-11-04 21:26:15 +0800

    add search api

--- src/api.js
+++ src/api.js
@@ -1,10 +1,14 @@
-import {searchSrv, mysql} from './utils';
+import {searchSrv, mysql, promiseToCallback} from './utils';
 
 export function sendJsonResponse(res, err, data={}, errStatus=500) {
   if (err) {
-    console.log(err);
-    err = err.toString();
-    res.status(errStatus).json({err});
+    if (typeof err === 'string') {
+      res.status(errStatus).json({err});
+    } else if (err instanceof Error) {
+      res.status(errStatus).json({err: err.message});
+    } else {
+      res.status(errStatus).json({err: 'Unknow'});
+    }
   } else {
     if (Array.isArray(data)) {
       if (data.length === 1) {
@@ -24,5 +28,19 @@ export function route(app) {
       }
       sendJsonResponse(res, err, results);
     });
+  });
+  app.get('/api/search/', (req, res) => {
+    const query = req.query;
+    const highlight = {fields: ['content']};
+    promiseToCallback(searchSrv.search.bind(searchSrv))('wenshu', {query, highlight}, (err, ret={hits: []}) => {
+      const hits = ret.hits.map((hit) => {
+        return {
+          id: hit.id,
+          content: hit.fragments.content.join(' '),
+          score: hit.score,
+        }
+      })
+      sendJsonResponse(res, err, { total_hits: ret.total_hits, hits });
+    })
   });
 }
